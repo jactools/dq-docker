ef3c0afb34769612a062833b59a290edf75e6b24
Jac <jac@nowhere.anywhere>
2025-11-20 22:02:05 +0100

    chore: update runit.sh to run dq_docker module (python -m dq_docker.run_adls_checkpoint)

--- runit.sh
+++ runit.sh
@@ -1,11 +1,16 @@
+
 # Assuming you have the .env file and Docker setup ready
+# Example using the Great Expectations CLI inside the container:
 # docker run -it --rm \
 #   --env-file .env \
 #   great-expectations-adls-spn \
 #   checkpoint run adls_checkpoint
 
+# Run the package as a module inside the container. This uses the
+# namespaced module `dq_docker.run_adls_checkpoint` now that the
+# runtime was moved into the `dq_docker` package.
 docker run -it --rm \
   --env-file .env \
   --volume "$(pwd)/great_expectations/uncommitted/data_docs":/usr/src/app/gx/uncommitted/data_docs \
   great-expectations-adls-spn \
-  python run_adls_checkpoint.py
+  python -m dq_docker.run_adls_checkpoint
